152e002ef56c795c9e5866bfd1d36e28cfc70829
Joel Klabo <joelklabo@gmail.com>
2016-05-02 22:14:05 -0700

    Adding screenshot to lane

--- BigTimerUITests/BigTimerUITests.swift
+++ BigTimerUITests/BigTimerUITests.swift
@@ -9,33 +9,20 @@
 import XCTest
 
 class BigTimerUITests: XCTestCase {
-        
+    
+    let app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        setupSnapshot(app)
+        app.launch()
+    }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        
-        let staticText = XCUIApplication().staticTexts["0"]
-        staticText.tap()
-        staticText.tap()
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertTrue(true)
+    func testScreenshot() {
+        app.staticTexts["0"].tap()
+        sleep(7)
+        snapshot("OneSecondTimer")
     }
     
 }

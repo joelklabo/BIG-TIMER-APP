c8e2fa70f1097b8034186f9ae5a4f5bf5aaff681
Joel Klabo <joelklabo@gmail.com>
2016-05-04 14:19:37 -0700

    Adding more TimeFormatter tests

--- BigTimerTests/TimeFormatterTests.swift
+++ BigTimerTests/TimeFormatterTests.swift
@@ -12,17 +12,21 @@ class TimeFormatterTests: XCTestCase {
     
     let formatter = TimeFormatter()
     
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
     func testThatTimeIsFormattedCorrectly() {
+        let tenSeconds = 10
+        let tenSecondsFormattedString = formatter.formatTime(tenSeconds)
+        XCTAssertTrue(tenSecondsFormattedString == "10")
+        
         let oneMinuteInSeconds = 60
         let oneMinuteFormattedString = formatter.formatTime(oneMinuteInSeconds)
         XCTAssertTrue(oneMinuteFormattedString == "1:00")
+        
+        let oneHourInSeconds = 60 * oneMinuteInSeconds
+        let oneHourFormattedString = formatter.formatTime(oneHourInSeconds)
+        XCTAssertTrue(oneHourFormattedString == "1:00:00")
+        
+        let oneHourOneMintuteAndTenSeconds = oneHourInSeconds + oneMinuteInSeconds + tenSeconds
+        let oneHourOneMinuteAndTenSecondsFormattedString = formatter.formatTime(oneHourOneMintuteAndTenSeconds)
+        XCTAssertTrue(oneHourOneMinuteAndTenSecondsFormattedString == "1:01:10")
     }
 }
